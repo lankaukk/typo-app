99b7fc3efa98f8fdd3e0ac905fe28f01e50afe6b
McKayla Lankau <lankaukk@yahoo.com>
2021-08-22 20:36:17 -0400

    generated serializers

---
+++ backend/app/serializers/compositiion_serializer.rb
@@ -0,0 +1,4 @@
+class CompositiionSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
---
+++ backend/app/serializers/element_serializer.rb
@@ -0,0 +1,4 @@
+class ElementSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end

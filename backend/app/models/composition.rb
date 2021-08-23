df1e0d1870f5b126c38cb81859513189e49189ec
McKayla Lankau <lankaukk@yahoo.com>
2021-08-22 23:09:16 -0400

    initial fetch of seed data from api to DOM

--- backend/app/models/composition.rb
+++ backend/app/models/composition.rb
@@ -1,3 +1,7 @@
 class Composition < ApplicationRecord
     has_many :elements
+
+    def created_at
+        attributes['created_at'].strftime("%b %d, %Y")
+      end
 end
--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,32 @@
-console.log("in index.js")
+const endPoint = "http://localhost:3000/api/v1/elements"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getElements()
+    
+})
+
+function getElements() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(elements => {
+        elements.data.forEach(elements => {
+            // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+            // debugger
+            // let newElements = new Elements(elements, elements.attributes)
+            
+            const elementsMarkup = `
+            <div data-id=${elements.id}>
+                <h1>${elements.attributes.characters}<h1>
+                <h3>${elements.attributes.placements}</h3>
+                <h3>${elements.attributes.colors}</h3>
+                <h3>${elements.attributes.font_family}</h3>
+                <p>Created by ${elements.attributes.composition.artist}</p>
+                <p>On ${elements.attributes.composition.created_at}</p>
+                <button data-id=${elements.id}>edit</button>
+            </div>
+            <br><br>`;
+
+            document.body.innerHTML += elementsMarkup
+        })
+    })
+}

3a62965e110dfa756b86a2b03227fd51fdaddcf2
McKayla Lankau <lankaukk@yahoo.com>
2021-08-23 12:59:17 -0400

    create button initial functionality

--- frontend/index.js
+++ frontend/index.js
@@ -29,3 +29,16 @@ function getElements() {
         })
     })
 }
+
+function makeComposition() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+    document.body.style.background = bgColor;
+
+    // document.getElementById('#elements-panel').innerHTML = bgColor;
+    console.log(bgColor);
+    
+}

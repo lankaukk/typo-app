1167286ad31c955c677af7f9d1652b1c5a681c12
McKayla Lankau <lankaukk@yahoo.com>
2021-08-22 20:24:30 -0400

    elements controller index create show

--- backend/app/controllers/api/v1/compositions_controller.rb
+++ backend/app/controllers/api/v1/compositions_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::CompositionsController < ApplicationController
 
     def index
-        @compositions = Composition.all
-        render json: @compositions
+        compositions = Composition.all
+        render json: compositions
     end
+
+    def create 
+        composition = Composition.new(composition_params)
+    end
+
+    def show 
+
+    end
+
+    private 
+
+    def composition_params
+        params.require(:composition).permit(:artist, :created_at)
+    end
+
 end
---
+++ backend/app/controllers/api/v1/elements_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::ElementsController < ApplicationController
+
+    def index
+        elements = Element.all
+        render json: elements
+    end
+
+    def create 
+        element = Element.new(element_params)
+        if element.save
+            render json: ElementSerializer.new(element), status: :accepted
+          else
+            render json: {errors: element.errors.full_messages}, status: :unprocessible_entity
+          end
+    end
+
+    def show 
+
+    end
+
+    private 
+
+    def element_params
+        params.require(:element).permit(:characters, :colors, :placements, :font_family, :composition_id)
+    end
+
+end
